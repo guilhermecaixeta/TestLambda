9a19d77464e87eeed439c24aeb23408c3a55c44c
Guilherme Lúcio <guilherme.lpcaixeta@gmail.com>
2019-04-08 14:24:53 -0300

    Add test source

---
+++ ConsoleTestApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleTestApplication.Service;
+using System;
+
+namespace ConsoleTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LambdaTest.CreateList();
+            var t1 = LambdaTest.LambdaDirectCall();
+            var t2 = LambdaTest.LambdaCompiled();
+            var t3 = LambdaTest.LambdaGenerated();
+            Console.WriteLine($"{t1}{Environment.NewLine}{t2}{Environment.NewLine}{t3}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTestApplication/Service/LambdaTest.cs
@@ -0,0 +1,88 @@
+﻿using Generic.Service.Attributes.Lambda;
+using Generic.Service.Entity.IFilter;
+using Generic.Service.Extensions.Commom;
+using Generic.Service.Extensions.Filter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ConsoleTestApplication.Service
+{
+    public static class LambdaTest
+    {
+        private static int _MAX = 1000000;
+        private static int _COMP = 150000;
+        public static List<FakeObject> testList { get; set; }
+
+
+        public static void CreateList()
+        {
+            testList = new List<FakeObject>();
+            foreach (int val in Enumerable.Range(1, _MAX))
+            {
+                testList.Add(new FakeObject() { value = val });
+            }
+        }
+
+        public static string LambdaDirectCall()
+        {
+            var timer = new Stopwatch();
+            int t = 0;
+            timer.Start();
+            for (int i = 0; i < 10000; i++)
+                t = testList.Where(x => x.value >= _COMP).Count();
+            timer.Stop();
+            return $"Total size {testList.Count()} - Total result {t} {Environment.NewLine}Direct call has executed in {timer.ElapsedMilliseconds} ms.";
+        }
+
+        public static string LambdaCompiled()
+        {
+            FilterFakeObject filter = new FilterFakeObject();
+            filter.value = _COMP;
+            Commom.SetSizeByLengthProperties("ConsoleTestApplication", "ConsoleTestApplication");
+            Commom.SaveOnCacheIfNonExists<FakeObject>();
+            Commom.SaveOnCacheIfNonExists<FilterFakeObject>();
+
+            var timer = new Stopwatch();
+            var queryable = testList.AsQueryable();
+            int t = 0;
+            var func = filter.GenerateLambda<FakeObject, FilterFakeObject>().Compile();
+            timer.Start();
+            for (int i = 0; i < 10000; i++)
+                t = queryable.Where(x => func(x)).Count();
+            timer.Stop();
+            return $"Total size {testList.Count()} - Total result {t} {Environment.NewLine}Compiled call has executed in {timer.ElapsedMilliseconds} ms.";
+        }
+
+        public static string LambdaGenerated()
+        {
+            FilterFakeObject filter = new FilterFakeObject();
+            filter.value = _COMP;
+            Commom.SetSizeByLengthProperties("ConsoleTestApplication", "ConsoleTestApplication");
+            Commom.SaveOnCacheIfNonExists<FakeObject>();
+            Commom.SaveOnCacheIfNonExists<FilterFakeObject>();
+
+            var timer = new Stopwatch();
+            int t = 0;
+            var queryable = testList.AsQueryable();
+            timer.Start();
+            for (int i = 0; i < 10000; i++)
+                t = queryable.Where(filter.GenerateLambda<FakeObject, FilterFakeObject>()).Count();
+            timer.Stop();
+            return $"Total size {testList.Count()} - Total result {t} {Environment.NewLine}Generated call has executed in {timer.ElapsedMilliseconds} ms.";
+        }
+    }
+
+
+    public class FakeObject
+    {
+        public int value { get; set; }
+    }
+
+    public class FilterFakeObject : IFilter
+    {
+        [LambdaGenerate(MethodOption = Generic.Service.Enums.Lambda.LambdaMethod.GreaterThanOrEqual)]
+        public int value { get; set; }
+    }
+}
